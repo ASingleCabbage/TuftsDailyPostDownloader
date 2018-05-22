b80281c914f8cdc81024a96167909f089ee8edac
Singular Cabbage <daniellangamma@gmail.com>
2018-05-22 17:55:17 -0400

    Added optional args for statusPrint func
    Also updated requirements.txt

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ class AppWindow(Ui_PostDownloader, QMainWindow):
         self.ui = Ui_PostDownloader()
         self.ui.setupUi(self)
 
-        defOutName = "output.json"
-        defOutFullPath = os.getcwd() + "\\" + defOutName
+        defOutName = 'output.json'
+        defOutFullPath = os.path.join(os.getcwd(), defOutName)
         self.ui.pathTextEdit.setPlainText(defOutFullPath)
 
         self.__query = queryBuilder(self)
@@ -27,23 +27,23 @@ class AppWindow(Ui_PostDownloader, QMainWindow):
         self.__query.downloader.postProcessor.dumpCompleteSignal.connect(self.dumpCompleteCallback)
 
         #translating button names to valid endpoint parameters
-        self.outputOptionDict = {"orderAuthorRB" : "author",
-                                 "orderDateRB" : "date",
-                                 "orderRelRB" : "relevance",
-                                 "orderTitleRB" : "title",
-                                 "orderIdRB" : "id",
-                                 "orderSlugRB" : "slug"}
+        self.outputOptionDict = {'orderAuthorRB' : 'author',
+                                 'orderDateRB' : 'date',
+                                 'orderRelRB' : 'relevance',
+                                 'orderTitleRB' : 'title',
+                                 'orderIdRB' : 'id',
+                                 'orderSlugRB' : 'slug'}
 
-        self.outputOrderDict = {"ascendingRB" : "asc", "descendingRB" : "desc"}
+        self.outputOrderDict = {'ascendingRB' : 'asc', 'descendingRB' : 'desc'}
 
         self.show()
 
     def setCatFilter(self, isEnabled):
-        assert(self.sender().objectName() == "catFilterGroup")
+        assert(self.sender().objectName() == 'catFilterGroup')
         self.__query.catFilterEnabled = isEnabled
 
     def setTagFilter(self, isEnabled):
-        assert(self.sender().objectName() == "tagFilterGroup")
+        assert(self.sender().objectName() == 'tagFilterGroup')
         self.__query.tagFilterEnabled = isEnabled
 
     def setOutputOrder(self):
@@ -53,41 +53,41 @@ class AppWindow(Ui_PostDownloader, QMainWindow):
         elif(objName in self.outputOptionDict):
             self.__query.outputOption = self.outputOptionDict[objName]
         else:
-            self.printStatus("Unknown object {} invoked setOutputOrder".format(objName), 2000)
+            self.printStatus('Unknown object {} invoked setOutputOrder'.format(objName), 2000)
 
     def setPostLimit(self, hasLimit):
-        assert(self.sender().objectName() == "onlyRB")
+        assert(self.sender().objectName() == 'onlyRB')
         self.__query.postLimitEnabled = hasLimit
 
     def setDateFilter(self, isEnabled):
         objName = self.sender().objectName()
-        if(objName == "postsAfterCheckBox"):
+        if(objName == 'postsAfterCheckBox'):
             self.__query.startDateEnabled = isEnabled
-        elif(objName == "postsBeforeCheckBox"):
+        elif(objName == 'postsBeforeCheckBox'):
             self.__query.endDateEnabled = isEnabled
         else:
-            self.printStatus("Unknown object {} invoked setDateFilter".format(objName), 2000)
+            self.printStatus('Unknown object {} invoked setDateFilter'.format(objName), 2000)
 
     def updateDateFilter(self):
         objName = self.sender().objectName()
-        if(objName == "afterDateEdit"):
+        if(objName == 'afterDateEdit'):
             self.__query.startDate = self.sender().date().toPyDate().isoformat()
-        elif(objName == "beforeDateEdit"):
+        elif(objName == 'beforeDateEdit'):
             self.__query.endDate = self.sender().date().toPyDate().isoformat()
         else:
-            self.printStatus("Unknown object {} invoked updateDateFilter".format(objName), 2000)
+            self.printStatus('Unknown object {} invoked updateDateFilter'.format(objName), 2000)
 
     def updatePostCount(self):
-        assert(self.sender().objectName() == "postCount")
+        assert(self.sender().objectName() == 'postCount')
         self.__query.postLimitCount = self.sender().value()
         return
 
     def updateCatFilter(self):
-        assert(self.sender().objectName() == "catTextEdit")
+        assert(self.sender().objectName() == 'catTextEdit')
         self.__query.catFilterRaw = self.sender().toPlainText()
 
     def updateTagFilter(self):
-        assert(self.sender().objectName() == "tagTextEdit")
+        assert(self.sender().objectName() == 'tagTextEdit')
         self.__query.tagFilterRaw = self.sender().toPlainText()
 
     #TODO: add file extension to save file dialog
@@ -95,12 +95,12 @@ class AppWindow(Ui_PostDownloader, QMainWindow):
         #welp I just copied and pasted this code ¯\_(ツ)_/¯
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"Create save file",os.getcwd(),"All Files (*)", options=options)
+        fileName, _ = QFileDialog.getSaveFileName(self,'Create save file',os.getcwd(),'All Files (*)', options=options)
         if fileName:
             self.ui.pathTextEdit.setPlainText(fileName)
             self.__query.outputFullPath = fileName
         else:
-            self.printStatus("Save file dialog canceled", 2000)
+            self.printStatus('Save file dialog canceled', 2000)
 
     def executeDownload(self):
         self.ui.progressBar.setValue(0)
@@ -113,20 +113,25 @@ class AppWindow(Ui_PostDownloader, QMainWindow):
             self.ui.execButton.setEnabled(True)
 
     def launchGithubPage(self):
-        webbrowser.open_new_tab("www.github.com")
+        #github repo
+        webbrowser.open_new_tab('https://github.com/ASingleCabbage/TuftsDailyPostDownloader')
         return
 
     def launchHelpPage(self):
-        webbrowser.open_new_tab("www.google.com")
+        #github wiki
+        webbrowser.open_new_tab('https://github.com/ASingleCabbage/TuftsDailyPostDownloader/wiki')
         return
 
-    def printStatus(self, message, duration):
-        self.ui.statusBar.showMessage(message, duration)
+    def printStatus(self, message, duration=None):
+        if(duration == None):
+            self.ui.statusBar.showMessage(message)
+        else:
+            self.ui.statusBar.showMessage(message, duration)
 
     def dumpCompleteCallback(self):
         self.ui.progressBar.setValue(100)
         self.ui.execButton.setEnabled(True)
-        self.printStatus("Download complete")
+        self.printStatus('Download complete')
 
 app = QApplication(sys.argv)
 w = AppWindow()
